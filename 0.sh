29dda08e7a63152ae42de0911e7f3ad81107e2f3
B.T.H_HACKER <tbabhav@gmail.com>
2025-07-19 13:59:16 +0530

    0.sh

---
+++ 0.sh
@@ -0,0 +1,2 @@
+ echo -e "\e[1;31m"                                    if ! [ -x "$(command -v figlet)" ]; then
+        echo 'Introducing ONE_TOOL'                       else                                                      figlet ONE_TOOL
